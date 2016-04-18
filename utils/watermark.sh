17fe847eb7126bf27135d2920cd81376d3c9ea73
Agustín <agustin@codecantor.com>
2016-04-18 11:08:33 +0200

    Add node5-ember image and utils

---
+++ utils/watermark.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+for command in  'chromium' \
+'ruby' 'gem' 'bundle' \
+'python' 'pip' \
+'node' 'npm' 'bower' 'ember'
+do
+{
+  #which $command > /dev/null && echo "`$command: $command -v` at `which $command`"
+  which $command > /dev/null && echo "$command: `$command --version`"
+}
+done
+
+exit 0 # Allways exit succesfully
