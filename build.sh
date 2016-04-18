96703978e8db31be13dc944947c565963989d305
Agustín <agustin@codecantor.com>
2016-04-18 12:28:04 +0200

    Remove utils from image folders

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ PREFIX="gitlab-ci-builds"
 NAME="node5-ember"
 TAG=latest
 
-
+# Copy utilities
 cp ./utils/add_ssh_private_key.sh ./$NAME/add_ssh_private_key.sh
 cp ./utils/watermark.sh ./$NAME/watermark.sh
 
